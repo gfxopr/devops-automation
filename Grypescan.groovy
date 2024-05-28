ac3826ae42555b9ce886e0f1ca9d2d282b67774e
owiniarski <42978603+gfxopr@users.noreply.github.com>
2024-05-28 10:36:44 +0200

    groovy

--- Grypescan.groovy
+++ Grypescan.groovy
@@ -1,1 +1,12 @@
-
+def checkForVulnerabilities(String imageName) {
+    def grypeScanner = Jenkins.instance.getExtensionList('io.jenkins.plugins.grypescanner.GrypeScanner')[0]
+    def scanResult = grypeScanner.scan(imageName)
+    
+    if (scanResult.hasVulnerabilities()) {
+        println "Vulnerabilities found in image: ${imageName}"
+        scanResult.getVulnerabilities().each { vulnerability ->
+            println "ID: ${vulnerability.getId()}, Severity: ${vulnerability.getSeverity()}"
+        }
+    } else {
+        println "No vulnerabilities found in image: ${imageName}"
+    }
